a48d0293b37695eaece44771fe1ce4cf73e237ee
AhmedSalama <ahmedsalamafcis@gmail.com>
2022-09-21 18:18:45 +0200

    adding timeer Task solution

--- clearinterval-stop-train/dist/script.js
+++ clearinterval-stop-train/dist/script.js
@@ -1,14 +1,28 @@
 let seconds = 0;
-
 let div = document.getElementById("myDiv");
 
 div.innerHTML = seconds;
 
-document.getElementById("start").onclick = function () {
-  setInterval(counter, 1000);
-};
+// document.getElementById("start").onclick = function () {
+//  timer= setInterval(counter, 1000);
+// };
+timer=clearInterval(setInterval(counter, 1000))
 
 function counter() {
   seconds += 1;
   div.innerHTML = seconds;
+}
+
+function startCounter(){
+    console.log("start")
+    timer= setInterval(counter, 1000);
+
+}
+function stopCounter(){
+    console.log("stop")
+    clearInterval(timer);
+}
+function resetCounter(){
+    seconds = 0;
+    div.innerHTML = seconds;
 }
